e4e88af3aecc872ab42f1e42948bb0af17b0a5b4
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2024-09-26 16:22:03 +0530

    Time: 8 ms (77.03%), Space: 58.2 MB (16.61%) - LeetHub

---
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumScore(int[] nums, int k) {
+        int l = k, r = k;
+        int min = nums[k], ass = nums[k];
+
+        while(l > 0 || r < nums.length-1)
+        {
+            int left = Integer.MIN_VALUE, right = Integer.MIN_VALUE;
+
+            if(l > 0) left = nums[l-1];
+            else left = 0;
+
+            if(r < nums.length-1) right = nums[r+1];
+            else right = 0;
+
+            if(left > right){
+                l--;
+                min = Math.min(min, left);
+            }
+            else{
+                r++;
+                min = Math.min(min, right);
+            }
+            ass = Math.max(ass, (r-l+1) * min);
+        }
+        return ass;
+    }
+}
