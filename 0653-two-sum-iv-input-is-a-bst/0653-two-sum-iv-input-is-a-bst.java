3ec73f81ab60066fd1efb587b3f4983ba67c5653
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2024-10-16 00:14:52 +0530

    Time: 3 ms (66.11%), Space: 45.1 MB (54.73%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -1,21 +1,14 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        HashSet<Integer> seen = new HashSet<>();
-        return inorder(root, k, seen);
+        HashSet<Integer> s = new HashSet<>();
+        return inorder(root, k, s);
     }
-
-    private boolean inorder(TreeNode root, int k, HashSet<Integer> seen) {
-        if (root == null) {
-            return false;
-        }
-
-        if (seen.contains(k - root.val)) {
-            return true;
-        }
-
-        seen.add(root.val);
-        return inorder(root.left, k, seen) || inorder(root.right, k, seen);
+    private boolean inorder(TreeNode root, int k, HashSet<Integer> s) {
+        if (root == null) return false;
+        if (s.contains(k - root.val)) return true;
+        s.add(root.val);
+        return inorder(root.left, k, s) || inorder(root.right, k, s);
     }
 }
