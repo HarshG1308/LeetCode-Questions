134acd7171cad93f211c24aa68683a12f2c00bd4
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2025-05-16 12:24:19 +0530

    Time: 139 ms (38.37%), Space: 44.7 MB (21.88%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -1,11 +1,21 @@
 class Solution {
     public int punishmentNumber(int n) {
-        int[] arr = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000};
-        int sum = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<=n) sum += arr[i]*arr[i];
-            else if(arr[i]>n) break;
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int sq = i * i;
+            if (isValid(Integer.toString(sq), 0, i, 0)) {
+                ans += sq;
+            }
         }
-        return sum;
+        return ans;
+    }
+
+    private boolean isValid(String s, int idx, int tgt, int sum) {
+        if (idx == s.length()) return sum == tgt;
+        for (int i = idx + 1; i <= s.length(); i++) {
+            int num = Integer.parseInt(s.substring(idx, i));
+            if (isValid(s, i, tgt, sum + num)) return true;
+        }
+        return false;
     }
 }
