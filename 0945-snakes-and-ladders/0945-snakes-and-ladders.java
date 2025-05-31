a05af0080c16ff2f54c61defd1f3124c5573d206
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2025-05-31 23:25:41 +0530

    Time: 5 ms (78.29%), Space: 44.5 MB (85.79%) - LeetHub

---
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        int dest = n*n;
+        boolean[] vis = new boolean[dest+1];
+        Queue<Integer> q = new LinkedList<>();
+        int level = 0;
+        q.offer(1); vis[1] = true;
+        while(!q.isEmpty()){
+            int s = q.size();
+            for(int i = 0; i < s; i++){
+                int r = q.poll();
+                if(r == dest) return level;
+                for(int j = r+1; j <= Math.min(r + 6, dest); ++j){
+                    int row = (j-1)/n;
+                    int col = (j-1)%n;
+                    if(row%2 == 1) col = n - 1 - col;
+                    row = n - 1 - row;
+                    int next = board[row][col] == -1 ? j : board[row][col];
+                    if(!vis[next]){
+                        vis[next] = true;
+                        q.offer(next);
+                    }
+                }
+            }
+            level++;
+        }
+        return -1;
+    }
+}
