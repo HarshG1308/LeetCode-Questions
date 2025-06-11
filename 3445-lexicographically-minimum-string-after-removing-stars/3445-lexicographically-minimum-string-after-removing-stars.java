0f05e349fe87d32426fa2d5ee95b41b3f598749d
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2025-06-11 15:51:32 +0530

    Time: 133 ms (61.11%), Space: 48.5 MB (52.06%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
@@ -2,35 +2,27 @@ import java.util.*;
 
 class Solution {
     public String clearStars(String s) {
-        // Store the characters added to the result
         StringBuilder sb = new StringBuilder();
-
-        // Map from character to list of indices in sb
         HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
         for (char c = 'a'; c <= 'z'; c++) {
             map.put(c, new ArrayList<>());
         }
-
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-
             if (ch == '*') {
-                // Remove the smallest lex character stored so far
                 for (char c = 'a'; c <= 'z'; c++) {
                     ArrayList<Integer> positions = map.get(c);
                     if (!positions.isEmpty()) {
-                        int index = positions.remove(positions.size() - 1); // remove last occurrence
-                        sb.setCharAt(index, '#'); // mark for deletion
+                        int index = positions.remove(positions.size() - 1);
+                        sb.setCharAt(index, '#');
                         break;
                     }
                 }
             } else {
                 sb.append(ch);
-                map.get(ch).add(sb.length() - 1); // store index of this character
+                map.get(ch).add(sb.length() - 1);
             }
         }
-
-        // Build final result excluding marked characters and '*'
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < sb.length(); i++) {
             char c = sb.charAt(i);
@@ -38,7 +30,6 @@ class Solution {
                 result.append(c);
             }
         }
-
         return result.toString();
     }
 }
