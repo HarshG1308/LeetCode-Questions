23b44e393370ff14a7af2808c1d5ece46246c84c
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2025-06-14 07:02:33 +0530

    Time: 135 ms (57.57%), Space: 174.7 MB (36.19%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -1,21 +1,26 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination){
-        boolean[] vis = new boolean[n]; 
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        boolean[] vis = new boolean[n];
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i= 0; i < n; i++) adj.add(new ArrayList<>());
-        for(int[] i : edges){
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+        for (int[] i : edges) {
             adj.get(i[0]).add(i[1]);
             adj.get(i[1]).add(i[0]);
         }
         return dfs(adj, vis, source, destination);
     }
-    boolean dfs(List<List<Integer>> adj, boolean[] vis, int src, int dest){
-        if(src == dest) return true;
+
+    boolean dfs(List<List<Integer>> adj, boolean[] vis, int src, int dest) {
+        if (src == dest)
+            return true;
         vis[src] = true;
         boolean ans = false;
-        for(int i : adj.get(src)){
-            if(!vis[i]) ans = dfs(adj, vis, i, dest);
-            if(ans) return true;
+        for (int i : adj.get(src)) {
+            if (!vis[i])
+                ans = dfs(adj, vis, i, dest);
+            if (ans)
+                return true;
         }
         return false;
     }
