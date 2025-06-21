f7bef9eef03c4d3d8ebab01b71b9c552788ae388
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2025-06-22 00:16:05 +0530

    Time: 5 ms (95.04%), Space: 45.9 MB (8.51%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.java
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.java
@@ -1,19 +1,16 @@
 class Solution {
     public int minimumDeletions(String word, int k) {
-        int n = word.length();
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c : word.toCharArray()){
-            if(!map.containsKey(c)) map.put(c, 1);
-            else map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        int ans = Integer.MAX_VALUE;
-        for(char c : map.keySet()){
+        int[] freq = new int[26];
+        for(char c : word.toCharArray()) freq[c-'a']++;
+        int ans = word.length();
+        for(int j = 0; j < 26; j++){
+            if(freq[j] == 0) continue;
+            int f = freq[j];
+            int max = f + k;
             int del = 0;
-            int base = map.get(c);
-            for(char ch : map.keySet()){
-                int f = map.get(ch);
-                if(f < base) del += f;
-                if(f > base + k) del += f - base - k;
+            for(int i = 0; i < 26; i++){
+                if(freq[i] < f) del += freq[i];
+                else if(freq[i] > max) del += freq[i]-max;
             }
             ans = Math.min(ans, del);
         }
