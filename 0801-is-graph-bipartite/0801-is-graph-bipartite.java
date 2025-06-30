7ea45dbe5ba8f06ff278cf5e5872cd51f55de461
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2025-06-30 11:13:32 +0530

    Time: 8 ms (7.32%), Space: 45.9 MB (7.07%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,27 +1,32 @@
 class Solution {
-    class Pair{
+    class Pair {
         int vtx;
         int dis;
-        public Pair(int vtx, int dis){
+
+        public Pair(int vtx, int dis) {
             this.vtx = vtx;
             this.dis = dis;
         }
     }
+
     public boolean isBipartite(int[][] graph) {
         Queue<Pair> q = new LinkedList<>();
         Map<Integer, Integer> vis = new HashMap<>();
-        for(int i = 0; i < graph.length; i++){
-            if(vis.containsKey(i)) continue;
+        for (int i = 0; i < graph.length; i++) {
+            if (vis.containsKey(i))
+                continue;
             q.add(new Pair(i, 0));
-            while(!q.isEmpty()){
+            while (!q.isEmpty()) {
                 Pair r = q.poll();
-                if(vis.containsKey(r.vtx)){
-                    if(vis.get(r.vtx) != r.dis) return false;
+                if (vis.containsKey(r.vtx)) {
+                    if (vis.get(r.vtx) != r.dis)
+                        return false;
                     continue;
                 }
                 vis.put(r.vtx, r.dis);
-                for(int j : graph[r.vtx]){
-                    if(!vis.containsKey(j)) q.add(new Pair(j, r.dis+1));
+                for (int j : graph[r.vtx]) {
+                    if (!vis.containsKey(j))
+                        q.add(new Pair(j, r.dis + 1));
                 }
             }
         }
